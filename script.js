707639b387bd447db30a52df2c4dd7d5eff5af07
Jordan Beck <oddlysuggestive@hotmail.com>
2020-01-25 12:36:43 -0500

    added slider, added projects
    slider okay, need tweaked, useable

--- script.js
+++ script.js
@@ -60,3 +60,20 @@ $('#navbarResponsive').on('hide.bs.collapse', function () {
   $('.navbar').removeClass('mobile-opened');
   $('.navbar').addClass('navbar-dark');
 });
+
+/*Multi Item Carousel */
+
+$('.multi-item-carousel').on('slide.bs.carousel', function (e) {
+  let $e = $(e.relatedTarget),
+      itemsPerSlide = 3,
+      totalItems = $('.carousel-item', this).length,
+      $itemsContainer = $('.carousel-inner', this),
+      it = itemsPerSlide - (totalItems - $e.index());
+  if (it > 0) {
+    for (var i = 0; i < it; i++) {
+      $('.carousel-item', this).eq(e.direction == "left" ? i : 0).
+        // append slides to the end/beginning
+        appendTo($itemsContainer);
+    }
+  }
+});
